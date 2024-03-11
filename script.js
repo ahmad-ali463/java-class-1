cf4c978572a3e027c7166b08dfb6d5e87da11317
Ahmad Ali Asif <spyali463@gmail.com>
2024-03-11 21:22:20 +0500

    diffcult

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ document.getElementById("on").onclick = function(){
 document.getElementById("off").onclick = function(){
  document.getElementById("light").src = "https://www.w3schools.com/js/pic_bulboff.gif"   
 }
+
+document.getElementById("as").onclick = function(){
+    document.getElementById("heading1").style.color = "blue"
+    
+}
+document.getElementById("heading1").innerHTML = "<span> This is a span</span> "
+
+// document.write("hello!")
+document.getElementById("aa").onclick = function () {
+    document.write(" <h1> hello </h>")
+}
+
+
