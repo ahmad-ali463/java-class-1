302b81313cd6515e1120c36f058f08b3a9f01ce8
Ahmad Ali Asif <spyali463@gmail.com>
2024-03-11 20:48:07 +0500

    javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+// alert("js file linked")
+document.getElementById("on").onclick = function(){
+ document.getElementById("light").src = "https://www.w3schools.com/js/pic_bulbon.gif"   
+}
+document.getElementById("off").onclick = function(){
+ document.getElementById("light").src = "https://www.w3schools.com/js/pic_bulboff.gif"   
+}
